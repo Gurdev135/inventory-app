64f751f3893884773f79bf9f279a7b5fff160128
Gurdev135 <gurdevgill135@gmail.com>
2023-03-10 11:48:33 +0000

    website updates

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,29 +1,90 @@
-import React, { useState, useEffect } from 'react';
-
-// import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import React, { useState } from 'react';
 
 export const App = () => {
+  const [showAllDataPopup, setShowAllDataPopup] = useState(false);
+  const [showDeleteDataPopup, setShowDeleteDataPopup] = useState(false);
+  const [showAddItemPopup, setShowAddItemPopup] = useState(false);
+  const [showUpdateItemPopup, setShowUpdateItemPopup] = useState(false);
 
-	return (
-		<main className='Adjust'>
-				<button className='Bt'>
-					All data
-				</button>
+  const handleShowAllDataPopup = () => {
+    setShowAllDataPopup(true);
+  };
 
-				<button className='Bt'>
-					Delete data
-				</button>
+  const handleHideAllDataPopup = () => {
+    setShowAllDataPopup(false);
+  };
 
-				<button className='Bt'>
-					Add item
-				</button>
+  const handleShowDeleteDataPopup = () => {
+    setShowDeleteDataPopup(true);
+  };
 
-				<button className='Bt'>
-					Update item
-				</button>
-		</main>
-	)
-}
+  const handleHideDeleteDataPopup = () => {
+    setShowDeleteDataPopup(false);
+  };
 
-module.exports = {App}
+  const handleShowAddItemPopup = () => {
+    setShowAddItemPopup(true);
+  };
+
+  const handleHideAddItemPopup = () => {
+    setShowAddItemPopup(false);
+  };
+
+  const handleShowUpdateItemPopup = () => {
+    setShowUpdateItemPopup(true);
+  };
+
+  const handleHideUpdateItemPopup = () => {
+    setShowUpdateItemPopup(false);
+  };
+
+  return (
+
+    <main className='Adjust'>
+		<div className='holder1'>	
+		<button className='Bt' onClick={handleShowAllDataPopup}>
+			All data
+		</button>
+		<button className='Bt' onClick={handleShowDeleteDataPopup}>
+			Delete data
+		</button>
+		
+		<button className='Bt' onClick={handleShowAddItemPopup}>
+			Add item
+		</button>
+		
+		<button className='Bt' onClick={handleShowUpdateItemPopup}>
+			Update item
+		</button>
+		</div>
+
+		<div className='holder2'>
+		{showAllDataPopup && (
+			<div className='popup'>
+				<button className='Bt2' onClick={handleHideAllDataPopup}>X</button>
+				<div>Data</div>
+			</div>
+		)}
+		{showDeleteDataPopup && (
+			<div className='popup'>
+				<button className='Bt2' onClick={handleHideDeleteDataPopup}>X</button>
+				<div>Data</div>
+			</div>
+		)}
+		
+		{showAddItemPopup && (
+			<div className='popup'>
+				<button className='Bt2' onClick={handleHideAddItemPopup}>X</button>
+				<div>Data</div>
+			</div>
+		)}
+		{showUpdateItemPopup && (
+			<div className='popup'>
+				<button className='Bt2' onClick={handleHideUpdateItemPopup}>X</button>			
+				<div>Data</div>
+			</div>
+		)}
+		</div>
+    </main>
+  );
+};
