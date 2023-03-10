cd8741194754e42135b3ea94e6145912e398a885
Gurdev135 <gurdevgill135@gmail.com>
2023-03-10 10:58:24 +0000

    updated react

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [sauces, setSauces] = useState([]);
-
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
-
-	useEffect(() => {
-		fetchSauces();
-	}, []);
-
 	return (
-		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+		<main className='Adjust'>
+				<button className='Bt'>
+					All data
+				</button>
+
+				<button className='Bt'>
+					Delete data
+				</button>
+
+				<button className='Bt'>
+					Add item
+				</button>
+
+				<button className='Bt'>
+					Update item
+				</button>
 		</main>
 	)
 }
+
+module.exports = {App}
